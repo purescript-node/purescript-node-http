bfd5979f09403aa8ebae2b2681af42cf09e0e814
Kirill Pertsev <kika@rackmaze.com>
2016-06-07 23:13:58 -0700

    Compatibility with Purescript 0.9.1

--- test/Main.purs
+++ test/Main.purs
@@ -1,23 +1,27 @@
 module Test.Main where
 
-import Prelude
+import Prelude (Unit, unit, pure, bind, void, (<>), ($))
 
 import Data.Foldable (foldMap)
-import Data.Options
 
-import Control.Monad.Eff.Console
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, log)
 
-import Node.HTTP
-import qualified Node.HTTP.Client as Client
+import Node.HTTP (HTTP, listen, createServer, setHeader, requestMethod, 
+                  requestURL, responseAsStream, requestAsStream, 
+                  setStatusCode)
+import Node.HTTP.Client as Client
 import Node.Stream
-import Node.Encoding
+import Node.Encoding (Encoding(..))
 
 foreign import stdout :: forall eff r. Writable r eff
 
+main::forall e. (Partial) => Eff (console::CONSOLE, http::HTTP|e) Unit
 main = do
   testBasic
   testHttps
 
+testBasic::forall e. (Partial) => Eff (http::HTTP, console::CONSOLE|e) Unit
 testBasic = do
   server <- createServer respond
   listen server 8080 $ void do
@@ -31,24 +35,26 @@ testBasic = do
     log (requestMethod req <> " " <> requestURL req)
     case requestMethod req of
       "GET" -> do
-        let html = foldMap (<> "\n")
+        let html = foldMap ((<>) "\n")
               [ "<form method='POST' action='/'>"
               , "  <input name='text' type='text'>"
               , "  <input type='submit'>"
               , "</form>"
               ]
         setHeader res "Content-Type" "text/html"
-        writeString outputStream UTF8 html(return unit)
-        end outputStream (return unit)
+        writeString outputStream UTF8 html(pure unit)
+        end outputStream (pure unit)
       "POST" -> void $ pipe inputStream outputStream
 
+testHttps::forall e. Eff (console::CONSOLE, http::HTTP|e) Unit
 testHttps =
   simpleReq "https://api.github.com"
 
+simpleReq::forall e. String -> Eff (console::CONSOLE, http::HTTP|e) Unit
 simpleReq uri = do
   log ("GET " <> uri <> ":")
   req <- Client.requestFromURI uri \response -> void do
     log "Response:"
     let responseStream = Client.responseAsStream response
     pipe responseStream stdout
-  end (Client.requestAsStream req) (return unit)
+  end (Client.requestAsStream req) (pure unit)
