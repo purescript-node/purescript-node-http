f15caa71a7d9b89b79740f0edc1e57f87fc6e29d
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-07-31 11:24:04 -0500

    Drop duplicate API for https server

--- src/Node/HTTP/Secure.js
+++ src/Node/HTTP/Secure.js
@@ -10,31 +10,3 @@ export const requestOptsImpl = (opts) => https.request(opts);
 export const getImpl = (url) => https.get(url);
 export const getUrlOptsImpl = (url, opts) => https.get(url, opts);
 export const getOptsImpl = (opts) => https.get(opts);
-
-export const closeAllConnectionsImpl = (hs) => hs.closeAllConnections();
-export const closeIdleConnectionsImpl = (hs) => hs.closeIdleConnections();
-export const headersTimeoutImpl = (hs) => hs.headersTimeout;
-export const setHeadersTimeoutImpl = (tm, hs) => {
-  hs.headersTimeout = tm;
-};
-
-export const maxHeadersCountImpl = (hs) => hs.maxHeadersCount;
-export const setMaxHeadersCountImpl = (c, hs) => {
-  hs.maxHeadersCount = c;
-};
-export const requestTimeoutImpl = (hs) => hs.requestTimeout;
-export const setRequestTimeoutImpl = (tm, hs) => {
-  hs.requestTimeout = tm;
-};
-export const maxRequestsPerSocketImpl = (hs) => hs.maxRequestsPerSocket;
-export const setMaxRequestsPerSocketImpl = (c, hs) => {
-  hs.maxRequestsPerSocket = c;
-};
-export const timeoutImpl = (hs) => hs.timeout;
-export const setTimeoutImpl = (c, hs) => {
-  hs.timeout = c;
-};
-export const keepAliveTimeoutImpl = (hs) => hs.keepAliveTimeout;
-export const setKeepAliveTimeoutImpl = (tm, hs) => {
-  hs.keepAliveTimeout = tm;
-};
